0484378aaab2efd1f9d009867f6130835b1bf597
anderwang <ander.wang@geminidata.com>
2023-05-15 13:16:12 +0800

    update 3b solution

--- maelstrom-multinode-broadcast/main.go
+++ maelstrom-multinode-broadcast/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 	"log"
 	"sync"
@@ -23,12 +22,11 @@ func main() {
 }
 
 type Server struct {
-	node                  *maelstrom.Node
-	messagesMap           map[int]interface{} // Hashset of golang version
-	messageLock           sync.RWMutex
-	nodeIDToKnownMessages map[string][]int // This server knows that for node ID n, it knows(seen) these messagesMap
+	node        *maelstrom.Node
+	messagesMap map[int]interface{} // Hashset of golang version
+	messageLock sync.RWMutex
 
-	topology     map[string]interface{}
+	topology     map[string][]string // mapping from the node ID to list of its neighbor IDs
 	topologyLock sync.RWMutex
 }
 
@@ -43,20 +41,18 @@ func (server *Server) handleBroadcastMessage(message maelstrom.Message) error {
 	server.messageLock.Lock()
 	if _, exists := server.messagesMap[messageValue]; exists {
 		server.messageLock.Unlock()
-		return nil
+		return server.node.Reply(message, map[string]any{
+			"type": "broadcast_ok",
+		})
 	}
 	server.messagesMap[messageValue] = struct{}{}
 	server.messageLock.Unlock()
 
 	// broadcast to neighbor node ID in the same cluster
-	for _, knownNodeIDInSameCluster := range server.node.NodeIDs() {
-		if knownNodeIDInSameCluster == message.Src || knownNodeIDInSameCluster == server.node.ID() {
-			continue
-		}
-
-		// Since goroutine only created at ending of loop, so knownNodeIDInSameCluster will only have one value
+	for _, neighborIDs := range server.topology[server.node.ID()] {
+		// Since goroutine only created at ending of loop, so neighborIDs will only have one value
 		// that is why we need a new local value at every iteration
-		targetNodeID := knownNodeIDInSameCluster
+		targetNodeID := neighborIDs
 		go func() {
 			if err := server.node.Send(targetNodeID, body); err != nil {
 				panic(err)
@@ -70,30 +66,30 @@ func (server *Server) handleBroadcastMessage(message maelstrom.Message) error {
 }
 
 func (server *Server) handleReadMessage(message maelstrom.Message) error {
-	server.messageLock.RLock()
+	server.messageLock.Lock()
 	// To extract keys as array from hashset in server.messagesMap
 	messages := make([]int, 0, len(server.messagesMap))
 	for key := range server.messagesMap {
 		messages = append(messages, key)
 	}
-	server.messageLock.RUnlock()
+	server.messageLock.Unlock()
 	return server.node.Reply(message, map[string]any{
 		"type":     "read_ok",
 		"messages": messages,
 	})
 }
 
+type topologyMessage struct {
+	Topology map[string][]string `json:"topology"`
+}
+
 func (server *Server) handleTopologyMessage(message maelstrom.Message) error {
-	var topologyMessageBody map[string]interface{}
+	var topologyMessageBody topologyMessage
 	if err := json.Unmarshal(message.Body, &topologyMessageBody); err != nil {
 		return err
 	}
-	topology, ok := topologyMessageBody["topology"].(map[string]interface{})
-	if !ok {
-		return fmt.Errorf("invalid topology format")
-	}
 	server.topologyLock.Lock()
-	server.topology = topology
+	server.topology = topologyMessageBody.Topology
 	server.topologyLock.Unlock()
 
 	return server.node.Reply(message, map[string]any{
